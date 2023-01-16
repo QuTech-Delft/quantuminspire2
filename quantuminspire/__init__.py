23f1881fd869bf6625f1293c3f2e56175ec74c2c
Olaf Schüsler <o.m.schusler@gmail.com>
2023-01-16 21:40:01 +0100

    [QI2-307] Fix coverage

--- quantuminspire/__init__.py
+++ quantuminspire/__init__.py
@@ -8,7 +8,7 @@ import sys
 if sys.version_info >= (3, 8):
     from importlib.metadata import PackageNotFoundError, version
 else:
-    from importlib_metadata import PackageNotFoundError, version
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
 
 
 try:
