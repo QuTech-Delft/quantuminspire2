46313d5f9ce25aba871acd57092edb3f9319e834
Harold Meerwaldt <haroldmeerwaldt@hotmail.com>
2024-06-18 14:12:49 +0200

    cleaned up hydrogen algorithm py file and jupyter notebook

--- examples/hydrogen_qiskit.py
+++ examples/hydrogen_qiskit.py
@@ -1,6 +1,6 @@
 # (H2) Hydrogen molecule ground state energy determined using VQE with a UCCSD-ansatz function.
-# Compared with Hartee-Fock energies and with energies calculated by NumPyMinimumEigensolver
 # This script is based on the Qiskit Chemistry tutorials
+import dataclasses
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from qiskit import transpile
 from qiskit.primitives import BackendEstimator
-from qiskit_algorithms import NumPyMinimumEigensolverResult, VQEResult
+from qiskit_algorithms import VQEResult
 from qiskit_algorithms.minimum_eigensolvers import VQE
 from qiskit_algorithms.optimizers import COBYLA
 from qiskit_nature.second_q.circuit.library import UCCSD, HartreeFock
@@ -24,7 +24,7 @@ from quantuminspire.util.api.quantum_interface import QuantumInterface
 
 @dataclass
 class _GroundStateEnergyResults:
-    result: VQEResult | NumPyMinimumEigensolverResult
+    result: VQEResult
     nuclear_repulsion_energy: float
 
 
@@ -44,32 +44,26 @@ def calculate_H0(backend: QuantumInspireBackend, distance: float = 0.735) -> _Gr
     estimator = BackendEstimator(backend=backend)
     initial_state = HartreeFock(n_spatial_orbitals, n_particles, mapper)
     ansatz = UCCSD(n_spatial_orbitals, n_particles, mapper, initial_state=initial_state)
-    optimizer = COBYLA(maxiter=1)
+    optimizer = COBYLA(maxiter=1)  # change to maxiter=100 for decent result
 
     algo = VQE(estimator, ansatz, optimizer)
     result = algo.compute_minimum_eigenvalue(qubit_op)
 
-    # print(f"{distance=}: nuclear_repulsion_energy={nuclear_repulsion_energy:.2f}, eigenvalue={result.eigenvalue:.2f}")
+    print(f"{distance=}: nuclear_repulsion_energy={nuclear_repulsion_energy:.2f}, eigenvalue={result.eigenvalue:.2f}")
     return _GroundStateEnergyResults(result, nuclear_repulsion_energy)
 
 
 def execute(qi: QuantumInterface) -> None:
-    # distances = np.arange(0.3, 2.5, 0.1)
-    distances = np.arange(0.3, 0.5, 0.1)
+    distances = np.arange(0.3, 2.5, 0.1)
     results = []
     for distance in distances:
         ground_state_energy_results = calculate_H0(backend=QuantumInspireBackend(qi), distance=distance)
-        # result = dataclasses.asdict(ground_state_energy_results)
-        result = {}
-        # result["eigenvalue"] = ground_state_energy_results.result.eigenvalue
-        # result["nuclear_repulsion_energy"] = ground_state_energy_results.nuclear_repulsion_energy
+        result = dataclasses.asdict(ground_state_energy_results)
         result["total_energy"] = ground_state_energy_results.nuclear_repulsion_energy + ground_state_energy_results.result.eigenvalue
         result["distance"] = distance
         results.append(result)
-        # print(f"{distance}, {result['total_energy']}")
 
     qi.results = {"results": results}
-    # print(results)
 
 
 def finalize(results: Any) -> dict[str, Any]:
@@ -97,7 +91,6 @@ if __name__ == "__main__":
         plt.plot(distances, energies, label=y_name)
     plt.legend()
     plt.show()
-    plt.savefig(Path("~/repositories/quantuminspire2/env/test.png").expanduser())
 
     # draw optimal circuit of first distance as text
     circuit = transpile(results[0]["result"].optimal_circuit, basis_gates=['ry', 'h', 'cx', 'x', 'sdg', 'rz', 's'])
