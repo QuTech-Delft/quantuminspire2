c56d14830b30e27db2ed853199d841f6fbf5fc87
Napoleon Cornejo <ncornejo@gmail.com>
2024-06-18 22:27:51 +0200

    [QI2-982] add unfair dice backbone script

--- examples/unfair_dice.py
+++ examples/unfair_dice.py
@@ -1,50 +1,14 @@
-"""
-
-Copyright 2024 QuTech (TNO, TU Delft)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-"""
-
-# Part of this code comes from ptetools:
-# https://github.com/eendebakpt/ptetools/tree/main
-
 from typing import Any, Dict, List
 from functools import partial
 from quantuminspire.sdk.models.circuit import Circuit
 from quantuminspire.util.api.quantum_interface import QuantumInterface
 from qiskit_algorithms.optimizers import COBYLA, SPSA
 from qiskit.circuit import Parameter
-from opensquirrel.ir import Qubit, Measure, Float, Bit
+from opensquirrel.ir import Qubit, Measure
 import numpy as np
 
 number_of_qubits = 2
-
-
-def counts_to_distr(counts) -> dict[int, float]:
-    """Convert Qiskit result counts to a dictionary
-
-    The dictionary has integers as keys, and pseudo-probabilities as values.
-    """
-    n_shots = sum(counts.values())
-    output_distr = {int(k, 2): v / n_shots for k, v in counts.items()}
-    return {k: output_distr[k] for k in sorted(output_distr)}
+number_of_parameters = 1
 
 
 class AverageDecreaseTermination:
@@ -102,53 +66,53 @@ class AverageDecreaseTermination:
                 return True
         return False
 
-def U(circuit_ir, q: Qubit, theta: float, phi: float, lamb: float):
-    '''
-        McKay decomposition of the U gate
-
-    :param self: circuit object
-    :param q: qubit
-    :param theta: angle
-    :param phi: angle
-    :param lamb: angle
-    :return: circuit object
-    '''
-    circuit_ir.Rz(q, Float(phi))
-    circuit_ir.Rx(q, Float(-np.pi/2))
-    circuit_ir.Rz(q, Float(theta))
-    circuit_ir.Rx(q, Float(np.pi / 2))
-    circuit_ir.Rz(q, Float(lamb))
-    return circuit_ir
-
 def generate_ansatz(params):
-    with Circuit(platform_name="spin-2", program_name="prgm1",
-                 number_of_qubits=2) as circuit:
-        U(circuit.ir, Qubit(0), *params[0:3])
-        U(circuit.ir, Qubit(1), *params[3:6])
+
+    with Circuit(platform_name="spin-2", program_name="prgm1", number_of_qubits=2) as circuit:
+        circuit.ir.H(Qubit(0))  #U
+        circuit.ir.H(Qubit(1))  #U
         circuit.ir.CZ(Qubit(0), Qubit(1))
-        U(circuit.ir, Qubit(0), *params[6:9])
-        U(circuit.ir, Qubit(1), *params[9:12])
+        circuit.ir.H(Qubit(0))  #U
+        circuit.ir.H(Qubit(1))  #U
         for ii in range(number_of_qubits):
-            circuit.ir.measure(Bit(ii), Qubit(ii))
+            Measure(ii, ii)
 
     return circuit
+    '''
+    qc = QuantumCircuit(number_of_qubits, number_of_qubits)
+    qc.u(*params[0:3], 0)
+    qc.u(*params[3:6], 1)
+    qc.cz(0, 1)
+    qc.u(*params[6:9], 0)
+    qc.u(*params[9:12], 1)
+    for ii in range(number_of_qubits):
+        qc.measure(ii, ii)
+    return qc
+    '''
+
 
 def objective_function(params, qi,
                        target_distribution, nshots=None):
-
     """Compares the output distribution of our circuit with
     parameters `params` to the target distribution."""
     qc = generate_ansatz(params)
-    execute_result = qi.execute_circuit(qc.content, nshots)
+
+    result = qi.execute_circuit(qc, nshots).result()
     # Convert the result to a dictionary with probabilities
-    output_distr = counts_to_distr(execute_result.results)
+    output_distr = counts_to_distr(result.get_counts())
     # Calculate the cost as the distance between the output
     # distribution and the target distribution
     cost = sum(
         abs(target_distribution.get(i, 0) - output_distr.get(i, 0))
-        for i in range(2 ** number_of_qubits)
+        for i in range(2 ** qc.num_qubits)
     )
     return cost
+
+
+
+#F = partial(
+#    objective_function, target_distribution=target_distribution, backend=backend, nshots=2400
+#)
 
 
 def data_callback(iteration: int, parameters: Any, residual: float) -> None:
@@ -160,31 +124,33 @@ def data_callback(iteration: int, parameters: Any, residual: float) -> None:
         residual: Current residual (value of the objective function)
 
     """
-    # do nothing for now
     pass
 
 
 def qiskit_callback(number_evaluations, parameters, value, stepsize, accepted):
+    """Callback method for Qiskit optimizers"""
+    #if self.show_progress:
+    print(f"#{number_evaluations}, {parameters}, {value}, {stepsize}, {accepted}")
     data_callback(number_evaluations, parameters, value)
 
 def execute(qi: QuantumInterface) -> None:
+    qc = generate_ansatz([Parameter(f"s_{i}") for i in range(number_of_parameters)])
 
-    optimizer = SPSA(maxiter=3, callback=qiskit_callback,
-                     termination_checker=AverageDecreaseTermination(N=35))
-
-    m = 2 ** number_of_qubits
-    p0 = np.random.random(m) + 0.2
-    p0 = p0 / np.sum(p0)
-    target_distribution = {k: p0[k] for k in range(m)}
-    F = partial(
-        objective_function, qi=qi, target_distribution=target_distribution, nshots=10
-    )
-
-    number_of_parameters = 12
     initial_parameters = .85 * np.random.rand(number_of_parameters, )
+
+    optimizer = SPSA(maxiter=500, callback=qiskit_callback,
+                     termination_checker=AverageDecreaseTermination(N=35))
     result = optimizer.minimize(fun=F, x0=initial_parameters)
 
-    return result
+
+
+    result = qi.execute_circuit(circuit, 4000)
+
+    counts = execute_circuits(list(range(1, qc.num_qubits + 1)), [qc], number_of_shots=4_000, save_data=False,
+                              name='objective_function')[0]
+
+    backend.run(qc, shots=10000).result().get_counts()
+
 
 
 def finalize(list_of_measurements: Dict[int, List[Any]]) -> Dict[str, Any]:
@@ -196,7 +162,6 @@ if __name__ == "__main__":
     target_distribution = {0: p, 1: 1 - p}
 
     # Run the individual steps for debugging
-    number_of_parameters = 12
     qc = generate_ansatz([Parameter(f"s_{i}") for i in range(number_of_parameters)])
     initial_parameters = .85 * np.random.rand(number_of_parameters, )
     print(qc.content)
